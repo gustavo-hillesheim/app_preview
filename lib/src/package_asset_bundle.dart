a3a7361cdc8c9d44f2bf218a155310bb504a62c2
Gustavo Hillesheim <guhillesheim@gmail.com>
2024-10-01 10:25:32 -0300

    Ensuring app_preview works in the web

--- lib/src/package_asset_bundle.dart
+++ lib/src/package_asset_bundle.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class PackageAssetBundle extends PlatformAssetBundle {
@@ -11,14 +13,27 @@ class PackageAssetBundle extends PlatformAssetBundle {
 
   @override
   Future<ByteData> load(String key) async {
-    if (key == 'AssetManifest.bin') {
-      final bytes = await super.load(key);
+    if (key == 'AssetManifest.bin' || key == 'AssetManifest.bin.json') {
+      var bytes = await super.load(key);
+      if (kIsWeb) {
+        final jsonData = utf8.decode(Uint8List.sublistView(bytes));
+        bytes = ByteData.sublistView(
+          base64.decode(json.decode(jsonData) as String),
+        );
+      }
       const codec = StandardMessageCodec();
       var data = codec.decodeMessage(bytes);
       if (data is Map) {
         data = _removePackagePrefixFromKeysOf(data);
       }
-      return codec.encodeMessage(data)!;
+      var result = codec.encodeMessage(data)!;
+      if (kIsWeb) {
+        final jsonData = json.encode(
+          base64.encode(Uint8List.sublistView(result)),
+        );
+        result = ByteData.sublistView(utf8.encode(jsonData));
+      }
+      return result;
     }
     return super.load(_ensureHasPackagePrefix(key));
   }
