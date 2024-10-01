2846d6c6d9f6b0d397cb3c0eaf897392e84ac788
Gustavo Hillesheim <guhillesheim@gmail.com>
2024-10-01 10:35:09 -0300

    Created _WebAssetManifestMessageCodec to simplify PackageAssetBundle.load

--- lib/src/package_asset_bundle.dart
+++ lib/src/package_asset_bundle.dart
@@ -15,24 +15,13 @@ class PackageAssetBundle extends PlatformAssetBundle {
   Future<ByteData> load(String key) async {
     if (key == 'AssetManifest.bin' || key == 'AssetManifest.bin.json') {
       var bytes = await super.load(key);
-      if (kIsWeb) {
-        final jsonData = utf8.decode(Uint8List.sublistView(bytes));
-        bytes = ByteData.sublistView(
-          base64.decode(json.decode(jsonData) as String),
-        );
-      }
-      const codec = StandardMessageCodec();
+      const codec =
+          kIsWeb ? _WebAssetManifestMessageCodec() : StandardMessageCodec();
       var data = codec.decodeMessage(bytes);
       if (data is Map) {
         data = _removePackagePrefixFromKeysOf(data);
       }
       var result = codec.encodeMessage(data)!;
-      if (kIsWeb) {
-        final jsonData = json.encode(
-          base64.encode(Uint8List.sublistView(result)),
-        );
-        result = ByteData.sublistView(utf8.encode(jsonData));
-      }
       return result;
     }
     return super.load(_ensureHasPackagePrefix(key));
@@ -52,5 +41,34 @@ class PackageAssetBundle extends PlatformAssetBundle {
       }
       return MapEntry(key, value);
     });
+  }
+}
+
+class _WebAssetManifestMessageCodec implements MessageCodec<Object?> {
+  const _WebAssetManifestMessageCodec();
+
+  @override
+  Object? decodeMessage(ByteData? message) {
+    if (message == null) {
+      return null;
+    }
+    final jsonData = utf8.decode(Uint8List.sublistView(message));
+    message = ByteData.sublistView(
+      base64.decode(json.decode(jsonData) as String),
+    );
+    return const StandardMessageCodec().decodeMessage(message);
+  }
+
+  @override
+  ByteData? encodeMessage(Object? message) {
+    if (message == null) {
+      return null;
+    }
+    var result = const StandardMessageCodec().encodeMessage(message)!;
+    final jsonData = json.encode(
+      base64.encode(Uint8List.sublistView(result)),
+    );
+    result = ByteData.sublistView(utf8.encode(jsonData));
+    return result;
   }
 }
